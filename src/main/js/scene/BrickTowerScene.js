6dc3b43edaae88a154a46175f4ef5b9fc7f3ab9d
Hannes Strydom <hannes.strydom@gmail.com>
2019-08-16 01:16:13 +1200

    The tower is getting some shape now
    Dynamic number of brick
    Adding brick to layer now in method and loaded in group
    Radius changes based on layer

--- src/main/js/scene/BrickTowerScene.js
+++ src/main/js/scene/BrickTowerScene.js
@@ -9,31 +9,70 @@ export class BrickTowerScene extends BaseScene {
 	}
 
 	setScene() {
-		let brickWall = BrickTowerScene.createBrickTower(8, 18, 5);
+		let brickWall = BrickTowerScene.createBrickTower(21, 10);
 		this.scene.add(brickWall);
 	}
 
-	static createBrickTower(numLayers = 4, bricksPerLayer = 10, radius = 5) {
+	static createBrickTower(numLayers = 4, minRadius = 5) {
 		const brickWall = new THREE.Group();
+
+		const maxRadius = minRadius * 1.3;
 
 		for (let layer = 0; layer < numLayers; layer++) {
 			const y = layer - (numLayers / 2);
+			let offset = (layer % 2 === 0);
 
-			for (let i = 0; i < bricksPerLayer; i++) {
-				let rot = (i / bricksPerLayer) * (Math.PI * 2);
+			let radius = minRadius;
 
-				if (layer % 2 === 0) {
-					rot += (0.5 / bricksPerLayer) * (Math.PI * 2);
-				}
+			const frac = layer/numLayers;
 
-				const x = Math.cos(rot) * radius;
-				const z = Math.sin(rot) * radius;
-
-				brickWall.add(BrickTowerScene.createBrick(x, y, z, rot + Math.PI/2));
+			if (frac >= 0.7) {
+				radius = maxRadius;
+			} else if (frac > 0.5) {
+				radius = ((maxRadius - minRadius) * (frac - 0.5) * 5) + minRadius
 			}
+
+			console.log(layer, frac, radius);
+
+			let brickLayer = this.createBrickLayer(radius, offset);
+			brickLayer.position.y = y;
+			brickWall.add(brickLayer);
+		}
+
+		for (let layer = numLayers; layer < numLayers + 2; layer++) {
+			const y = layer - (numLayers / 2);
+
+			let brickLayer = this.createBrickLayer(maxRadius, false, 1);
+			brickLayer.position.y = y;
+			brickWall.add(brickLayer);
 		}
 
 		return brickWall;
+	}
+
+	static createBrickLayer(radius, offset, skip = 0) {
+		const bricksPerLayer = Math.round((radius + 1) * Math.PI);
+
+		const brickLayer = new THREE.Group();
+
+		// for (let i = 0; i < bricksPerLayer; i += skip + 1) {
+		for (let i = 0; i < bricksPerLayer; i++) {
+			let rot = (i / bricksPerLayer) * (Math.PI * 2);
+
+			if (offset) {
+				rot += (0.5 / bricksPerLayer) * (Math.PI * 2);
+			}
+
+			const x = Math.cos(rot) * radius;
+			const y = 0;
+			const z = Math.sin(rot) * radius;
+
+			brickLayer.add(BrickTowerScene.createBrick(x, y, z, rot + Math.PI / 2));
+			if (skip > 0) {
+				i+= skip;
+			}
+		}
+		return brickLayer;
 	}
 
 	static createBrick(x, y, z, rotation) {
